a7efcaab84a35ab6a77b6e09ecfde330d36d5c5b
mingjie-gmj <mingjie.gmj@gmail.com>
2021-03-10 14:25:43 +0800

    update

--- problems/p_00021_merge_two_lists/merge_two_lists.go
+++ problems/p_00021_merge_two_lists/merge_two_lists.go
@@ -1,1 +1,29 @@
 package p_00021_merge_two_lists
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummyHdr := &ListNode{}
+	p := dummyHdr
+
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			p.Next = l1
+			l1 = l1.Next
+		} else {
+			p.Next = l2
+			l2 = l2.Next
+		}
+		p = p.Next
+	}
+
+	if l1 != nil {
+		p.Next = l1
+	} else {
+		p.Next = l2
+	}
+	return dummyHdr.Next
+}
--- problems/p_00021_merge_two_lists/merge_two_lists_test.go
+++ problems/p_00021_merge_two_lists/merge_two_lists_test.go
@@ -1,1 +1,30 @@
 package p_00021_merge_two_lists
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_mergeTwoLists(t *testing.T) {
+	type args struct {
+		l1 *ListNode
+		l2 *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{"case1", args{&ListNode{1, &ListNode{2, &ListNode{4, nil}}}, &ListNode{1, &ListNode{3, &ListNode{4, nil}}}}, &ListNode{1, &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{4, nil}}}}}}},
+		{"case2", args{nil, nil}, nil},
+		{"case3", args{nil, &ListNode{0, nil}}, &ListNode{0, nil}},
+		{"case4", args{&ListNode{1, nil}, nil}, &ListNode{1, nil}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mergeTwoLists(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("mergeTwoLists() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
