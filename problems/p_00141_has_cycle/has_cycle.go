8a2f0d0bab1d5bd197c37601d4389243e84f8001
mingjie-gmj <mingjie.gmj@gmail.com>
2021-03-13 14:53:07 +0800

    update

--- problems/p_00141_has_cycle/has_cycle.go
+++ problems/p_00141_has_cycle/has_cycle.go
@@ -18,3 +18,17 @@ func hasCycle(head *ListNode) bool {
 	}
 	return false
 }
+
+func hasCycle1(head *ListNode) bool {
+	slow ,fast := head, head
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+
+}
