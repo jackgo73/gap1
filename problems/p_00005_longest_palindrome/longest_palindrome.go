9a5108f8a5e00bed85bbddeb3ff010a41568fc47
mingjie-gmj <mingjie.gmj@gmail.com>
2021-03-14 17:44:06 +0800

    update

--- problems/p_00005_longest_palindrome/longest_palindrome.go
+++ problems/p_00005_longest_palindrome/longest_palindrome.go
@@ -22,3 +22,28 @@ func palindrome(s string, left int, right int) (int, int) {
 	}
 	return left + 1, right - 1
 }
+
+
+func longestPalindrome1(s string) string {
+	start, end := 0, 0
+	for i := 0; i < len(s); i++ {
+		left1, right1 := getEdge(s, i, i)
+		left2, right2 := getEdge(s, i, i+1)
+		if right2-left2 > end-start {
+			start, end = left2, right2
+		}
+		if right1-left1 > end-start {
+			start, end = left1, right1
+		}
+	}
+
+	return s[start: end+1]
+}
+
+func getEdge(s string, left int, right int) (int, int) {
+	for left >= 0 && right < len(s) && s[left] == s[right] {
+		left--
+		right++
+	}
+	return left + 1, right - 1
+}
