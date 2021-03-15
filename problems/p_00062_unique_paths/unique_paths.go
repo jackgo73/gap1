ae645a11e66551c91d244480b1cd074b0cc69e4f
mingjie-gmj <mingjie.gmj@gmail.com>
2021-03-15 22:33:55 +0800

    update

---
+++ problems/p_00062_unique_paths/unique_paths.go
@@ -0,0 +1,23 @@
+package p_00062_unique_paths
+
+func uniquePaths(m, n int) int {
+	dp := make([][]int, m)
+	for i := range dp {
+		dp[i] = make([]int, n)
+		dp[i][0] = 1
+	}
+	for j := 0; j < n; j++ {
+		dp[0][j] = 1
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	return dp[m-1][n-1]
+}
+
+// m=3 n=7
+// 1  1  1  1  1  1  1
+// 1  2  3  4  5  6  7
+// 1  3  6  10 15 21 28
---
+++ problems/p_00062_unique_paths/unique_paths_test.go
@@ -0,0 +1,24 @@
+package p_00062_unique_paths
+
+import "testing"
+
+func Test_uniquePaths(t *testing.T) {
+	type args struct {
+		m int
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"case1", args{3, 7}, 28},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := uniquePaths(tt.args.m, tt.args.n); got != tt.want {
+				t.Errorf("uniquePaths() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
